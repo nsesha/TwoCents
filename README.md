42c96f0e44b35438daa90c0246ff1cbeec56a462
nsesha <seshadri.ns@gmail.com>
2017-03-05 22:10:14 -0800

    Initial Commit for TwoCents

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # TwoCents
+
+TwoCents is a Tip Calculator application for iOS submitted as the [pre-assignment](http://courses.codepath.com/snippets/intro_to_ios/prework) requirement for Codepath.
+
+Time spent: 10 hours
+
+Completed:
+
+* [x] Required: Tip Calculator functionality for default and custom %age of tip
+* [x] Required: Settings page to change the default tip currency.
+* [x] Optional: Specify # of persons for tip
+* [x] Optional: Remembering the bill amount across app restarts (if <10mins)
+* [x] Additional: Making sure the keyboard is always visible and the bill amount is always the first responder. 
+      This way the user doesn't have to tap anywhere to use this app. Just launch the app and start typing.
+* [x] Additional: Theming the Settings page like the main application page.
+* [x] Additional: Also remembering the tip percentage across app restarts.
+
+Notes:
+This app has been testing on iphone 7 simulator
+
+
+![Video Walkthrough](twoCents_animation.gif)
+
+GIF created with [LiceCap](http://www.cockos.com/licecap/).
---
+++ TwoCents/ViewController.swift
@@ -0,0 +1,91 @@
+//
+//  ViewController.swift
+//  TwoCents
+//
+//  Created by admin on 3/4/17.
+//  Copyright © 2017 Seshadri N. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var customTipField: UITextField!
+    @IBOutlet weak var numPersonField: UITextField!
+    @IBOutlet weak var perPersonLabel1: UILabel!
+    @IBOutlet weak var perPersonLabel2: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+        /*
+        let formatter = NumberFormatter()
+        
+        for locale in NSLocale.availableLocaleIdentifiers {
+            print("\(locale)")
+            formatter.locale = NSLocale(localeIdentifier: locale) as Locale!
+            print("\(formatter.currencyCode) =  \(formatter.currencySymbol)")
+            let symbol = String(format: "\(formatter.currencySymbol)")
+            print("\t\t\(symbol)")
+        }
+        
+        */
+    }
+
+    @IBAction func calculateTip(_ sender: Any) {
+        let currencies = ["en_US", "en_JE", "en_EE", "en_INR", "en_CN"]
+        let tipPercentages = [0.18, 0.2, 0.25]
+        let bill = Double(billField.text!) ?? 0
+        let tipIndex = tipControl.selectedSegmentIndex
+        var tipPercent = tipPercentages[0]
+        
+        let numPersons = Int(numPersonField.text!) ?? 0
+        
+        if tipIndex == 3 {
+            customTipField.isHidden = false
+            tipPercent = Double(customTipField.text!) ?? 0
+            tipPercent = tipPercent / 	100
+        } else {
+            customTipField.isHidden = true
+            tipPercent = Double(tipPercentages[tipIndex])
+        }
+        
+        var tip = bill * Double(tipPercent)
+        var total = bill + tip
+        
+        if numPersons > 1 {
+            perPersonLabel1.isHidden = false
+            perPersonLabel2.isHidden = false
+            tip = tip / Double(numPersons)
+            total = total / Double(numPersons)
+        } else {
+            perPersonLabel1.isHidden = true
+            perPersonLabel2.isHidden = true
+        }
+        
+        let symbol = getSymbol(currency: currencies[4])
+        tipLabel.text = String(format: "\(symbol) %.2f", tip)
+        totalLabel.text = String(format: "\(symbol) %.2f", total)
+    }
+
+    func getSymbol(currency: String) -> String{
+        let formatter = NumberFormatter()
+        
+        formatter.locale = NSLocale(localeIdentifier: currency) as Locale!
+        return formatter.currencySymbol
+    }
+    
+}
